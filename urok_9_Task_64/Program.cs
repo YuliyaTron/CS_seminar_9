ef5e8c54512d0f232835d167829cfaa83e40b6a6
yuliya tron <tron_yv@mail.ru>
2022-08-28 11:37:02 +0300

    apdated urok_9_Task_64/

--- urok_9_Task_64/Program.cs
+++ urok_9_Task_64/Program.cs
@@ -1,14 +1,19 @@
-﻿// Задача 64
-// Задайте значения M и N. Напишите программу, которая 
-// выведет все натуральные числа в промежутке от M до N.
-// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
-// M = 4; N = 8. -> ""4,5, 6, 7, 8""
+﻿/* Задача 64
+Задайте значения M и N. Напишите программу, которая 
+выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+M = 4; N = 8. -> ""4,5, 6, 7, 8""
+*/
 
 Console.Clear();
-Console.Write("Введите первое число M: ");
-int numM = int.Parse(Console.ReadLine());
-Console.Write("Введите второе число N: ");
-int numN = int.Parse(Console.ReadLine());
+Console.Write("Введите первое число M : ");
+int numM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+Console.Write("Введите второе число N :");
+int numN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
 NaturalNumber(numM, numN);
 
 void NaturalNumber(int m, int n)
@@ -27,5 +32,5 @@ void NaturalNumber(int m, int n)
 
    if (n == m) Console.Write($"{n} ");
 }
-
+Console.WriteLine();
 
